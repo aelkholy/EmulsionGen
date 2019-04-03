a269b308fd9ff2ed53346aefb601b8d7876dd309
aelkholy <alyks42@gmail.com>
2019-04-03 11:12:20 -0700

    main compiles

--- app/Main.hs
+++ app/Main.hs
@@ -3,13 +3,14 @@ module Main where
 import FileLoader
 -- FROM STACKAGE
 import System.Environment
+import System.IO
 
 main :: IO ()
 main = do
     args <- getArgs
     case args of
-      [] -> error "must supply a file to open"
-      [arg] -> do handle <- openFile arg ReadMode
+      [] -> error "must supply file"
+      [arg] -> do 
+        handle <- openFile arg ReadMode
+        putStrLn "asdf"
       _ -> error "too many arguments"
-    
-    
