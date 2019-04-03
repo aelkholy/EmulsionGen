b389161c8dca5df64618c745d423a230d1c7ff36
aelkholy <alyks42@gmail.com>
2019-04-03 14:47:30 -0700

    Types refactor

--- src/SilverGelatin.hs
+++ src/SilverGelatin.hs
@@ -14,46 +14,50 @@ module SilverGelatin where
 -- transitions are mixing things
 -- labelling function is the properties (propositions) at each state. Temperature, reactant quantities, etc.
 
--- Emulsion
+------------------
 
-type Temperature = Float
+-- Emulsion Definitions --
 
-data Quantity = Grams {amount :: Float} | Ml {amount :: Float, concentration :: Float} deriving (Eq, Show, Read)  -- quantity, quantity / perc
+data Unit = GRAM | MILLILITER
+data Quantity = QUANTITY {amount :: Double, unit :: Unit}
+type Temperature = Double
 
-type Water = Quantity
-type KI = Quantity
-type KBr = Quantity
-type NaCl = Quantity
+data Salt = KI { sQuantity :: Quantity}
+  | KBr { sQuantity :: Quantity}
+  | NaCl { sQuantity :: Quantity}
+data Silver = SILVER { vQuantity :: Quantity}
+data Acid = ACID { aQuantity :: Quantity}
+data ChemicalModifier = CHEMICALMODIFIER { mQuantity :: Quantity, mWeight :: Double}
 
-type Silver = Quantity
-data ChemicalModifier = ChemicalModifier {name :: String, molWeight :: Float, quantity :: Quantity} deriving (Eq, Show, Read) -- Amt, Name, Molecular weight
-type Acid = ChemicalModifier
-type Modifier = ChemicalModifier
+data Solution = SOLUTION {
+  salts :: [Salt],
+  ag :: [Silver],
+  acids :: [Acid],
+  other :: [ChemicalModifier],
+  temp :: Temperature
+}
 
-data Salt = KI | KBr | NaCl deriving (Eq, Show, Read, Ord) -- order from solubility and reaction preference.
+class Chemical a where
+  molecularWeight :: a -> Double
 
-data Chemical = Salt Salt | Acid Acid | Modifier Modifier | Silver Silver | Water Water
+instance Chemical Salt where
+  molecularWeight (KI _) = 166.0028
+  molecularWeight (KBr _) = 119.002
+  molecularWeight (NaCl _) = 58.44
 
-modifierMW :: Modifier -> Float
-modifierMW = molWeight
+instance Chemical Silver where
+  molecularWeight (SILVER _)  = 169.87
 
-molecularWeight :: Chemical -> Float
-molecularWeight (Salt x) = (case x of KI -> 166.0028
-                                      KBr -> 119.002
-                                      NaCl -> 58.44)
-molecularWeight (Acid x) = modifierMW x
-molecularWeight (Modifier x) = modifierMW x 
-molecularWeight (Silver x) = 169.87
-molecularWeight (Water x) = 18.01528
+instance Chemical ChemicalModifier where
+  molecularWeight (CHEMICALMODIFIER _ m) = m
 
--- solubility :: Salt -> Float
+-- Kripke Definitions --
 
--- Kripke
-data State = Gelatin  | Precipitation | Wash | PrecipitationWash | AfterRipening | Done deriving (Eq, Show, Read)
+data State = Unmixed  | Precipitation | Wash | PrecipitationWash | AfterRipening | Done deriving (Eq, Show, Read)
 
 type Initial = State
 
 data Transistion = Transition {time :: Float, transition :: State -> State}
 
-data Emulsion = Emulsion [State] State [Transistion] (State -> [Chemical]) (State -> Temperature)
--- States, Initial State, Transitions, Labeling, Labeling
+data Emulsion = Emulsion [State] State [Transistion] (State -> [Solution])
+-- States, Initial State, Transitions, Labeling
