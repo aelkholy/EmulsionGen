e2ae7d5dd35b1b30238b79cbb67d0577bd3b4761
aelkholy <alyks42@gmail.com>
2019-05-07 17:24:20 -0700

    decided on 'emulsion' type format

--- src/Emulsion.hs
+++ src/Emulsion.hs
@@ -1,14 +1,24 @@
+{-# LANGUAGE DeriveFoldable #-}
 module Emulsion (
   Emulsion
 ) where
 
 --
+import Physics
 import qualified Solution             as S
-import qualified Stages.Precipitation as P
-import qualified Stages.Digestion     as D
 
 -- What questions do I want to ask?
 -- What stages does an emulsion go through
 -- Because later I want to infer properties and impute based off of these
+-- {duration :: Minute, temperature :: Temperature, finals :: [ChemicalModifier]}
 
-data Emulsion = EMULSION {precipitation :: [P.Precipitation], digestion :: D.Digestion, finalSolution :: S.Solution}
+data Emulsion a = PRECIPITATION { duration :: Minute, temperature :: Temperature, solution :: a, emulsion :: Emulsion a}
+  | DIGESTION { duration :: Minute, temperature :: Temperature, solution :: a, emulsion :: Emulsion a}
+  | FINISHED a 
+  deriving (Foldable, Show)
+
+-- -- Grow tree
+-- chemicalLab :: S.Solution -> [S.Step] -> Emulsion S.Solution
+-- chemicalLab solution (s:teps) = 
+
+-- -- Reduce tree
